bc3e72569dc3f3b652b431c8f0a80191ef605336
Toussaint BOOS <tboos@e1r1p3.42.fr>
2016-03-06 09:47:25 +0100

    mise norme en cours

--- ft_ls.h
+++ ft_ls.h
@@ -6,23 +6,23 @@
 /*   By: tboos <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/03/03 11:10:56 by tboos             #+#    #+#             */
-/*   Updated: 2016/03/06 07:43:49 by tboos            ###   ########.fr       */
+/*   Updated: 2016/03/06 09:18:34 by tboos            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_LS_H
 # define FT_LS_H
 
-#include <grp.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <dirent.h>
-#include <errno.h>
-#include <string.h>
-#include <pwd.h>
-#include <time.h>
-#include <stdio.h>
-#include <sys/ioctl.h>
+# include <grp.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <dirent.h>
+# include <errno.h>
+# include <string.h>
+# include <pwd.h>
+# include <time.h>
+# include <stdio.h>
+# include <sys/ioctl.h>
 
 # define MIN_A 0000001
 # define MAG_A 0000002
@@ -46,23 +46,29 @@
 # define FREE1 ft_freegiveone
 # define MIN_R_ARG (MIN_R & arg)
 
-typedef struct		s_dirent
+typedef struct dirent	t_direntori;
+typedef struct stat		t_stat;
+typedef struct			s_dirent
 {
-	struct dirent	*data;
-	struct stat		stat;
-	struct s_dirent	*next;
-}					t_dirent;
+	t_direntori			*data;
+	t_stat				stat;
+	struct s_dirent		*next;
+}						t_dirent;
 
-void		ft_putmajmin(struct stat stat);
-void		ft_printcol(t_dirent *lst, size_t nb_col, size_t ldisp, size_t jump);
-void		ft_printperm(t_dirent *lst);
-void		ft_deldot(t_dirent **lst, int mode);
-void		ft_printl(t_dirent *lst);
-long		ft_cmpls(t_dirent *turtle, t_dirent *rabbit, int arg);
-int			ft_create_d_list(t_dirent **begin, DIR *dir, char *name, int arg);
-void		ft_free_dirent_lst(t_dirent *lst);
-void		ft_revlst(t_dirent **begin);
-void		ft_lstdir(char *name, int arg, t_dirent *lst, int *end);
-int			ft_ls(char **av, int ac, int arg, int i);
-void		ft_list_insert(t_dirent **begin, t_dirent *rabbit, int arg);
+void					ft_putmajmin(struct stat stat);
+void					ft_printcol(t_dirent *lst, size_t nb_col, size_t ldisp,
+						size_t jump);
+void					ft_printperm(t_dirent *lst);
+void					ft_deldot(t_dirent **lst, int mode);
+void					ft_printl(t_dirent *lst);
+long					ft_cmpls(t_dirent *turtle, t_dirent *rabbit, int arg);
+int						ft_create_d_list(t_dirent **begin, DIR *dir,
+						char *name, int arg);
+void					ft_free_dirent_lst(t_dirent *lst);
+void					ft_revlst(t_dirent **begin);
+void					ft_lstdir(char *name, int arg, t_dirent *lst, int *end);
+int						ft_ls(char **av, int ac, int arg, int i);
+void					ft_list_insert(t_dirent **begin, t_dirent *rabbit,
+						int arg);
+
 #endif
